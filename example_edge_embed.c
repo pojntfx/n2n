fa1965fc81b3b1c2a5573f07cb5ab5c49db57d7a
Felix Pojtinger <felix@pojtinger.com>
2019-12-29 03:17:35 +0100

    Remove dependency on n2n_priv_config_t for configuration

--- example_edge_embed.c
+++ example_edge_embed.c
@@ -5,23 +5,16 @@ static int keep_on_running;
 int main()
 {
     n2n_edge_conf_t conf;
-    n2n_priv_config_t ec;
     tuntap_dev tuntap;
     n2n_edge_t *eee;
     int rc;
 
-    // TODO: Stop using `ec`
-    memset(&ec, 0, sizeof(ec));
-    ec.daemon = 0;                                                       // Don't daemonize
-    snprintf(ec.device_mac, sizeof(ec.device_mac), "DE:AD:BE:EF:01:10"); // Set mac address
-    ec.groupid = 0;                                                      // Run as root
-    ec.userid = 0;                                                       // Run as root
-    snprintf(ec.ip_addr, sizeof(ec.ip_addr), "10.0.0.1");                // Set ip address
-    snprintf(ec.ip_mode, sizeof(ec.ip_mode), "static");                  // IP mode; static|dhcp
-    ec.mtu = DEFAULT_MTU;                                                // MTU to use
-    snprintf(ec.netmask, sizeof(ec.netmask), "255.255.255.0");           // Netmask to use
-    snprintf(ec.tuntap_dev_name, sizeof(ec.tuntap_dev_name), "edge0");   // Name of the device to use
-    // ec.got_s = 1; // Whether it has a subnet (internal property)
+    char *device_mac = "DE:AD:BE:EF:01:10"; // Set mac address
+    char *ip_addr = "10.0.0.1";             // Set ip address
+    char *ip_mode = "static";               // IP mode; static|dhcp
+    int mtu = DEFAULT_MTU;                  // MTU to use
+    char *netmask = "255.255.255.0";        // Netmask to use
+    char *tuntap_dev_name = "edge0";        // Name of the device to use
 
     edge_init_conf_defaults(&conf);
     conf.allow_p2p = 1;                                                                      // Whether to allow peer-to-peer communication
@@ -35,18 +28,16 @@ int main()
     conf.mgmt_port = N2N_EDGE_MGMT_PORT;                                                     // Edge management port (5644 by default)
     conf.register_interval = 1;                                                              // Interval for both UDP NAT hole punching and supernode registration
     conf.register_ttl = 1;                                                                   // Interval for UDP NAT hole punching through supernode
-    // conf.sn_ip_array; // internal property; edge_conf_add_supernode handles this
-    // conf.sn_num; // internal property; edge_conf_add_supernode handles this
-    edge_conf_add_supernode(&conf, "localhost:1234"); // Supernode to connect to
-    conf.tos = 16;                                    // Type of service for sent packets
-    conf.transop_id = N2N_TRANSFORM_ID_TWOFISH;       // Use the twofish encryption
+    edge_conf_add_supernode(&conf, "localhost:1234");                                        // Supernode to connect to
+    conf.tos = 16;                                                                           // Type of service for sent packets
+    conf.transop_id = N2N_TRANSFORM_ID_TWOFISH;                                              // Use the twofish encryption
 
     if (edge_verify_conf(&conf) != 0)
     {
         return -1;
     }
 
-    if (tuntap_open(&tuntap, ec.tuntap_dev_name, ec.ip_mode, ec.ip_addr, ec.netmask, ec.device_mac, ec.mtu) < 0)
+    if (tuntap_open(&tuntap, tuntap_dev_name, ip_mode, ip_addr, netmask, device_mac, mtu) < 0)
     {
         return -1;
     }
