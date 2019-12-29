088853323e052430d1522430b05101d24358eedf
Felix Pojtinger <felix@pojtinger.com>
2019-12-29 03:09:41 +0100

    Set remaining settable conf struct fields

--- example_edge_embed.c
+++ example_edge_embed.c
@@ -10,6 +10,7 @@ int main()
     n2n_edge_t *eee;
     int rc;
 
+    // TODO: Stop using `ec`
     memset(&ec, 0, sizeof(ec));
     ec.daemon = 0;                                                       // Don't daemonize
     snprintf(ec.device_mac, sizeof(ec.device_mac), "DE:AD:BE:EF:01:10"); // Set mac address
@@ -23,22 +24,22 @@ int main()
     // ec.got_s = 1; // Whether it has a subnet (internal property)
 
     edge_init_conf_defaults(&conf);
-    // conf.allow_p2p;
-    // conf.allow_routing;
+    conf.allow_p2p = 1;                                                                      // Whether to allow peer-to-peer communication
+    conf.allow_routing = 1;                                                                  // Whether to allow the edge to route packets to other edges
     snprintf((char *)conf.community_name, sizeof(conf.community_name), "%s", "mycommunity"); // Community to connect to
-    // conf.disable_pmtu_discovery;
-    // conf.drop_multicast;
-    // conf.dyn_ip_mode;
-    conf.encrypt_key = "mysecret"; // Secret to decrypt & encrypt with
-    // conf.local_port;
-    // conf.mgmt_port;
-    // conf.register_interval;
-    // conf.register_ttl;
-    // conf.sn_ip_array;
-    // conf.sn_num;
-    // conf.tos;
-    conf.transop_id = N2N_TRANSFORM_ID_TWOFISH;       // Use the twofish encryption
+    conf.disable_pmtu_discovery = 1;                                                         // Whether to disable the path MTU discovery
+    conf.drop_multicast = 0;                                                                 // Whether to disable multicast
+    conf.dyn_ip_mode = 0;                                                                    // Whether the IP address is set dynamically (see IP mode; 0 if static, 1 if dynamic)
+    conf.encrypt_key = "mysecret";                                                           // Secret to decrypt & encrypt with
+    conf.local_port = 0;                                                                     // What port to use (0 = any port)
+    conf.mgmt_port = N2N_EDGE_MGMT_PORT;                                                     // Edge management port (5644 by default)
+    conf.register_interval = 1;                                                              // Interval for both UDP NAT hole punching and supernode registration
+    conf.register_ttl = 1;                                                                   // Interval for UDP NAT hole punching through supernode
+    // conf.sn_ip_array; // internal property; edge_conf_add_supernode handles this
+    // conf.sn_num; // internal property; edge_conf_add_supernode handles this
     edge_conf_add_supernode(&conf, "localhost:1234"); // Supernode to connect to
+    conf.tos = 16;                                    // Type of service for sent packets
+    conf.transop_id = N2N_TRANSFORM_ID_TWOFISH;       // Use the twofish encryption
 
     if (edge_verify_conf(&conf) != 0)
     {
